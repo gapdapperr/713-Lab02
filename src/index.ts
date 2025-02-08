e45f5a76fd5407ff8c421fd13b40a27f2ec8aa3d
Gap <puri.pongsomboonsuk@gmail.com>
2025-02-08 13:35:13 +0700

    Task03.1

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,4 @@
-let x: number | string = 10;
-x = 43
-
-if (typeof x === 'string') {
-    console.log('x is a string')
-} else if (typeof x === 'number') {
-    console.log('x is a number')
-} else {
-    console.log('x is neither a string nor a number')
+const add = (a,b) => {
+    return a + b;
 }
+console.log(add(1,2));
