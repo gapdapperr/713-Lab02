0c5feb9656dcc216c9a43ace2948ff204b7c624a
Gap <puri.pongsomboonsuk@gmail.com>
2025-02-08 14:11:37 +0700

    Task04.5

--- src/function.ts
+++ src/function.ts
@@ -2,3 +2,8 @@ export const add =(a: number, b: number): string => {
     const result = a + b;
     return result.toString();
 }
+
+export const subtract = (a: number, b: number): string => {
+    const result = a - b;
+    return result.toString();
+}
--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,9 @@ const findMax = (number: number[]) :string => {
 let number: number[] = [1,2,3,4,5]; 
 console.log(findMax(number));
 
-import { add } from "./function";
+import { add, subtract } from "./function";
 
 const result = add(1,2) + 0;
+const result2 = subtract(1,2) + 0;
 console.log(result, 'type of result:', typeof result);
-
+console.log(result2, 'type of result2:', typeof result2);
