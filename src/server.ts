5c050ad38fa0fdef089ad48dcee3cc25eabd9a6f
Gap <puri.pongsomboonsuk@gmail.com>
2025-02-10 21:13:48 +0700

    Task08.9

--- src/server.ts
+++ src/server.ts
@@ -6,13 +6,10 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
-app.get("/test", (req, res) => {
-  let returnObj = {
-    name: "test",
-    age: 20,
-    address: "Thai",
-  };
-  res.send(returnObj);
+app.get("/test", (req: Request, res: Response) => {
+  const id = req.query.id;   
+    const output = `id: ${id}`;
+    res.send(output);
 });
 
 app.listen(port, () => {
