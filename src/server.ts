ce9f80672012aa8b12b51135e8656168e77c7692
Gap <puri.pongsomboonsuk@gmail.com>
2025-02-10 21:32:09 +0700

    Task09.1-09.4

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,87 @@ import express, { Request, Response } from 'express'
 const app = express()
 const port = 3000
 
+interface Event {
+  id: number;
+  category: string;
+  title: string;
+  description: string;
+  location: string;
+  date: string;
+  time: string;
+  petsAllowed: boolean;
+  organizer: string;
+}
+
+const events: Event[] = [
+  {
+        id: 1,
+        category: "Music",
+        title: "Concert",
+        description: "A live concert",
+        location: "London",
+        date: "2021-07-01",
+        time: "19:00",
+        petsAllowed: false,
+        organizer: "Live Nation",
+    },
+    {
+        id: 2,
+        category: "Music",
+        title: "Festival",
+        description: "A music festival",
+        location: "Manchester",
+        date: "2021-08-01",
+        time: "12:00",
+        petsAllowed: false,
+        organizer: "Festival Republic",
+    },
+    {
+        id: 3,
+        category: "Theatre",
+        title: "Play",
+        description: "A live play",
+        location: "Birmingham",
+        date: "2021-09-01",
+        time: "19:00",
+        petsAllowed: false,
+        organizer: "National Theatre",
+    },
+    {
+        id: 4,
+        category: "Theatre",
+        title: "Musical",
+        description: "A live musical",
+        location: "Liverpool",
+        date: "2021-10-01",
+        time: "19:00",
+        petsAllowed: false,
+        organizer: "Cameron Mackintosh",
+    },
+    {
+        id: 5,
+        category: "Comedy",
+        title: "Stand-up",
+        description: "A live stand-up comedy show",
+        location: "Leeds",
+        date: "2021-11-01",
+        time: "19:00",
+        petsAllowed: false,
+        organizer: "Live Nation",
+    },
+    {
+        id: 6,
+        category: "Comedy",
+        title: "Improv",
+        description: "A live improv comedy show",
+        location: "Glasgow",
+        date: "2021-12-01",
+        time: "19:00",
+        petsAllowed: false,
+        organizer: "Live Nation",
+    }];
+
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
@@ -10,6 +91,10 @@ app.get("/test", (req: Request, res: Response) => {
   const id = req.query.id;   
     const output = `id: ${id}`;
     res.send(output);
+});
+
+app.get("/events", (req, res) => {
+  res.json(events);
 });
 
 app.listen(port, () => {
