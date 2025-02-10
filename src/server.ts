0b4119f1ad1a98dbec46736783cc5c706956a378
Gap <puri.pongsomboonsuk@gmail.com>
2025-02-10 21:09:47 +0700

    Task08.6-08.7

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,10 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
+app.get('/test', (req, res) => {
+    res.send('Hello World! 3')
+  })
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
 })
