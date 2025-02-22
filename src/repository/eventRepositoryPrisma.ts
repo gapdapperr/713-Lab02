d59d0385ce7cc37d7c8e855d2c32d4e887b45cb3
Gap <puri.pongsomboonsuk@gmail.com>
2025-02-22 13:32:20 +0700

    Task3 removing return type

--- src/repository/eventRepositoryPrisma.ts
+++ src/repository/eventRepositoryPrisma.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import type { Event } from '../models/event';
+import type { Event } from "../models/event";
 
 const prisma = new PrismaClient();
 
@@ -11,19 +11,21 @@ export function getAllEvents(): Promise<Event[]> {
   return prisma.event.findMany();
 }
 
-export function getEventById(id: number): Promise<Event | null> {
+export function getEventById(id: number) {
   return prisma.event.findUnique({ where: { id } });
 }
 
 export function addEvent(newEvent: Event): Promise<Event> {
-  return prisma.event.create({ data: {
-    category: newEvent.category,
-    title: newEvent.title,
-    description: newEvent.description,
-    location: newEvent.location,
-    date: newEvent.date,
-    time: newEvent.time,
-    petsAllowed: newEvent.petsAllowed,
-    organizer: newEvent.organizer
-  } });
+  return prisma.event.create({
+    data: {
+      category: newEvent.category,
+      title: newEvent.title,
+      description: newEvent.description,
+      location: newEvent.location,
+      date: newEvent.date,
+      time: newEvent.time,
+      petsAllowed: newEvent.petsAllowed,
+      organizer: newEvent.organizer,
+    },
+  });
 }
