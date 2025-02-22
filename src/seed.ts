2830e2ac8dc212b6c1cf42009f877789dca55a43
Gap <puri.pongsomboonsuk@gmail.com>
2025-02-22 16:11:44 +0700

    Task7.7 - Task7.8

--- src/seed.ts
+++ src/seed.ts
@@ -2,9 +2,9 @@ import { createEvents } from "./db/createEvents";
 import { PrismaClient } from "@prisma/client";
 import { createParticipants } from "./db/createParticipants";
 const prisma = new PrismaClient();
-createParticipants().catch((e) => {
-    console.error(e);
-    process.exit(1);
-}).finally(async () => {
-    await prisma.$disconnect();
-});
+async function main() {
+  await createEvents();
+  await createParticipants();
+}
+
+main()
